4b4a5b886d7ed5171b986859c5440ebee3defc97
Zmajzlik <artur.ulfig2@gmail.com>
2018-04-19 08:58:59 +0200

    Sprawozdanie 7

---
+++ Sprawozdanie7/ConsoleApplication1/VectorMath.cpp
@@ -0,0 +1,13 @@
+// Vector Math Functions
+// OpenGL SuperBible gltools library
+// Richard S. Wright Jr.
+// One note of interest. These REALLY should be inlined. I'm sorry I was too stupid at the time
+// to figure out how to get inlining to work on all the platforms and different compilers I use.
+// this seemed simpler than a ton of #ifdef's all over the place. Watch the web site as I may
+// make frequent improvments t
+#include "stdafx.h"
+#include <cstdlib>
+#include "gltools_extracted.h"
+#include <math.h>
+
+// Adds two vectors together
